f4d005b0259c0c264f532601616c142ddbf86485
Kim Schlesinger <kschlesinger@digitalocean.com>
2021-07-27 15:42:35 -0600

    add LastLogin field

--- api/main.go
+++ api/main.go
@@ -20,6 +20,7 @@ type User struct {
 	AvatarURL         string `json:"avatar_url"`
 	Company           string `json:"company"`
 	MostRecentComment string `json:"most_recent_comment"`
+	LastLogin string `json:"last_login"`
 }
 
 var users = []User{
@@ -29,7 +30,9 @@ var users = []User{
 		LastName:          "Schlesinger",
 		AvatarURL:         "https://community-cdn-digitalocean-com.global.ssl.fastly.net/variants/a79PCNZWVqKWL4pZZytLEXmW/1b33f0ae5d4693bf57c52014e04c03ab70f276df2ccd0b8ddde11732686ee1a9",
 		Company:           "Digital Ocean",
-		MostRecentComment: "Hey, I'm giving a tech talk on July 28, 2021. Join me!"},
+		MostRecentComment: "Hey, I'm giving a tech talk on July 28, 2021. Join me!",
+		LastLogin: 			"Tue, 27 Jul 2021 21:39:08 +0000"
+	},
 	{Id: 654651652,
 		UserName:          "masonegger",
 		FirstName:         "Mason",
@@ -37,6 +40,7 @@ var users = []User{
 		AvatarURL:         "https://community-cdn-digitalocean-com.global.ssl.fastly.net/variants/EXJi5mGhdvFYbTxQ8Sfkdwfd/1b33f0ae5d4693bf57c52014e04c03ab70f276df2ccd0b8ddde11732686ee1a9",
 		Company:           "Digital Ocean",
 		MostRecentComment: "Python!",
+		LastLogin: 			"Mon, 24 May 2021 21:39:08 +0000",
 	},
 	{Id: 654651653,
 		UserName:          "chrisoncode",
@@ -45,6 +49,7 @@ var users = []User{
 		AvatarURL:         "https://community-cdn-digitalocean-com.global.ssl.fastly.net/variants/FvZ5kCncEfUQXbUkiSrNyJrW/1b33f0ae5d4693bf57c52014e04c03ab70f276df2ccd0b8ddde11732686ee1a9",
 		Company:           "Digital Ocean",
 		MostRecentComment: "JavaScript!",
+		LastLogin: 			"Sun, 25 Jul 2021 16:33:08 +0000"
 	},
 	{Id: 654651654,
 		UserName:          "mattipv4",
@@ -53,6 +58,7 @@ var users = []User{
 		AvatarURL:         "https://community-cdn-digitalocean-com.global.ssl.fastly.net/variants/KyhncqSAFFeF3ULjGDX3MgC7/1b33f0ae5d4693bf57c52014e04c03ab70f276df2ccd0b8ddde11732686ee1a9",
 		Company:           "Digital Ocean & CloudFlare",
 		MostRecentComment: "Rails, CDNs, Community!",
+		LastLogin: 			"Wed, 28 Jul 2021 10:05:29 +0000"
 	},
 }
 
